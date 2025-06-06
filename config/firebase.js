25e3a2730c77e6476f64c4f44e89df2e7205fa1c
Yudhriz <riskyyudha678@gmail.com>
2025-06-06 17:13:47 +0700

    fix: memperbaiki kode SDK

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,18 @@
 import admin from "firebase-admin";
+import fs from "fs"; // Modul untuk membaca file
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+// Render akan meletakkan Secret File di path ini
+const SECRET_FILE_PATH = "/etc/secrets/serviceAccount.json";
 
+// Baca konten file JSON dan parse
+const serviceAccount = JSON.parse(fs.readFileSync(SECRET_FILE_PATH, "utf8"));
+
+// Inisialisasi Firebase Admin
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+  console.log("Inisialisasi Firebase dari Secret File berhasil!");
 }
 
 const auth = admin.auth();
